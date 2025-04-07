63ea3857b39780539857eb06d282de69991306d3
steiner-254 <steineralvin3@gmail.com>
2025-04-07 08:33:48 -0700

    Learning CEHv13

--- gpush.bat
+++ gpush.bat
@@ -1,11 +1,5 @@
-:: Steps To Create The Simple Automation Workflow Below - Works Only For Windows VS Code Terminal:
-:: 1. Create a file with extension `.bat` -> gpush.bat
-:: 2. Copy the code below into the file/Add your own commands if you wish
-:: 3. Open the Windows terminal on VS Code and run the command `.\gpush.bat` to execute the script - `gpush.bat` is our file name, feel free to customize
-:: 4. Successfully Automated
-:: -> Millionaire Vibes Inshallah! 🚀
-
 @echo off
+pushd \\wsl.localhost\Ubuntu\home\steiner254\CEHv13-Short-Notes
 echo Adding all files to Git...
 git add .
 
@@ -18,8 +12,4 @@ git commit -m "Learning CEHv13"
 echo Pushing to remote repository...
 git push
 
-echo "" Millionaire Hacker Vibes Inshallah! Happy Hacking <3 ""
-echo "" Millionaire Hacker Vibes Inshallah! Happy Hacking <3 ""
-echo "" Millionaire Hacker Vibes Inshallah! Happy Hacking <3 ""
-echo "" Millionaire Hacker Vibes Inshallah! Happy Hacking <3 ""
-echo "" Millionaire Hacker Vibes Inshallah! Happy Hacking <3 ""
+popd
